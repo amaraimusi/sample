c8db875cf2a40e1759ea9944429fc2efceb13d84
k_uehara <amaraimusi@gmail.com>
2019-07-17 08:33:13 +0900

    jquery_uploadの修正：セキュリティ上の問題を修正

--- js/jquery_upload_sample/upload.php
+++ js/jquery_upload_sample/upload.php
@@ -1,4 +1,9 @@
 <?php
+
+if($_SERVER["REMOTE_ADDR"] !='localhost'){
+    echo 'This sample is localhost only';
+    die();
+}
     move_uploaded_file($_FILES["upload_file"]["tmp_name"], "img/" . $_FILES["upload_file"]["name"]);
     echo "img/" .$_FILES["upload_file"]["name"];
 ?>
