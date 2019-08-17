61d68519002ef404c4aabab45ea1c7d9f3a619bb
user <user@DESKTOP-LP8D1KP>
2019-08-18 08:24:16 +0900

    JavaScriptのサンプル：RangeYm.jsのバージョンアップ

--- js/a012/range_ym_ex/RangeYm.js
+++ js/a012/range_ym_ex/RangeYm.js
@@ -2,7 +2,7 @@
  * 年月による日付範囲入力 | RangeYm.js
  * @date 2019-8-17
  * @license MIT
- * @version 1.0.1
+ * @version 1.1.3
  */
 class RangeYm{
 	
@@ -45,10 +45,11 @@ class RangeYm{
 		this._addChangeYmTb(this.ymTb); // 年月テキストのチェンジイベント
 		
 		// デフォルト値の取得とセット
-		this.param['def'] = this.tDiv.attr('data-def');
-		if(this.param.def !=null){
-			this.setYm(this.param.def); // 年月をセット
-		}
+		this.param['def_ym'] = this.tDiv.attr('data-def-ym');
+		this.param['def1'] = this.tDiv.attr('data-def1');
+		this.param['def2'] = this.tDiv.attr('data-def2');
+		
+		this.setYm(this.param.def_ym, this.param.def1, this.param.def2); // 年月をセット
 
 	}
 
@@ -64,7 +65,7 @@ class RangeYm{
 		if(param['field'] == null) throw new Error('field is empty');
 		if(param['wamei'] == null) throw new Error('wamei is empty');
 		if(param['field_from'] == null) param['field_from'] = param.field + '1';
-		if(param['field_to'] == null) param['field_from'] = param.field + '2';
+		if(param['field_to'] == null) param['field_to'] = param.field + '2';
 
 		return param;
 	}
@@ -104,11 +105,11 @@ class RangeYm{
 		let field_to = this.param.field_to; // 終わり日付のフィールド名
 		
 		let html = `
-	<input type="month" class="range_ym_tb"  placeholder="「${wamei}」の年月" title="年月を選択すると「>」ボタンの先にその月の日付範囲が入力されます。" />	
+	<input type="month" id="${field}_ym" class="range_ym_tb"  placeholder="「${wamei}」の年月" title="年月を選択すると「>」ボタンの先にその月の日付範囲が入力されます。" />	
 	<button type="button" class="range_ym_dtl_btn btn btn-default btn-xs" title="日付を範囲入力する部分を表示します。"><span class="glyphicon glyphicon-chevron-right"></span></button>
 	<div class="range_ym_range_div" style="display:inline-block">
-		<input type="date" class="${field_from} range_ym_date_from "  placeholder="「${wamei}」の範囲【始め】" title="「${wamei}」範囲の始め日付です。" />	
-		<input type="date" class="${field_to} range_ym_date_to "  placeholder="「${wamei}」の範囲【終わり】" title="「${wamei}」範囲の終わり日付です。" />	
+		<input type="date" id="${field_from}" class="${field_from} kjs_inp range_ym_date_from "  placeholder="「${wamei}」の範囲【始め】" title="「${wamei}」範囲の始め日付です。" />	
+		<input type="date" id="${field_to}" class="${field_to} kjs_inp range_ym_date_to "  placeholder="「${wamei}」の範囲【終わり】" title="「${wamei}」範囲の終わり日付です。" />	
 		<div class="range_ym_err text-danger"></div>
 	</div>
 		`;
@@ -133,23 +134,33 @@ class RangeYm{
 		
 		dtlBtn.click((evt)=>{
 			
-			var d = this.rngDiv.css('display');
-			if(d==null | d=='none'){
-				let f_show_btn_name = this._getDtlBtnName(0);
-				this.dtlBtn.val(f_show_btn_name);
-				this.tDiv.css('display','block');
-				this.rngDiv.show(100);
-				
-			}else{
-				let f_show_btn_name = this._getDtlBtnName(1);
-				this.dtlBtn.val(f_show_btn_name);
-				this.tDiv.css('display','inline-block');
-				this.rngDiv.hide(100);
-				
-			}
-
+			this._toggleRngDiv(); // 範囲区分の表示切替
+		
 		});
 	}
+	
+	
+	/**
+	 * 範囲区分の表示切替
+	 */
+	_toggleRngDiv(){
+		var d = this.rngDiv.css('display');
+		if(d==null | d=='none'){
+			let f_show_btn_name = this._getDtlBtnName(0);
+			this.dtlBtn.val(f_show_btn_name);
+			this.tDiv.css('display','block');
+			this.rngDiv.show(100);
+			
+		}else{
+			let f_show_btn_name = this._getDtlBtnName(1);
+			this.dtlBtn.val(f_show_btn_name);
+			this.tDiv.css('display','inline-block');
+			this.rngDiv.hide(100);
+			
+		}
+	}
+	
+	
 	
 	
 	/**
@@ -230,14 +241,76 @@ class RangeYm{
 	/**
 	 * 年月をセット
 	 * @param string ym 年月
+	 * @param string date1 始め日付（省略時は年月から自動セット）
+	 * @param string date2 終わり日付（省略時は年月から自動セット）
 	 */
-	setYm(ym){
-		ym = this._toYyyyMm(ym); // 年月文字列をyyyy-mm型に変換する
+	setYm(ym, date1, date2){
+
+		ym = this._yyyymm(ym); // 年月文字列をyyyy-mm型に変換する
 		this.ymTb.val(ym);
+
+		let rng_div_show_flg = false; // 範囲区分表示フラグ
+
+		// 始め日付をセットする
+		if(this._empty(date1)){
+			if(this._empty(ym)){
+				date1 = '';
+			}else{
+				date1 = ym + '-01'; // 始め日付
+			}
+		}else{
+			rng_div_show_flg = true;
+		}
+		date1 = this._yyyymmdd(date1); // yyyy-mm-dd型に変換する
+		this.fromInp.val(date1);
+
+		// 終わり日付をセットする
+		if(this._empty(date2)){
+			if(this._empty(ym)){
+				date2 = '';
+			}else{
+				date2 = this._getMonthEndDate(date1); // 月末日を取得
+			}
+		}else{
+			rng_div_show_flg = true;
+		}
+		date2 =this. _yyyymmdd(date2); // yyyy-mm-dd型に変換する
+		this.toInp.val(date2);
+
+		// 範囲区分の表示
+		if(rng_div_show_flg){
+			this._toggleRngDiv(); // 範囲区分の表示切替
+		}
 		
-		// 年月テキストのチェンジイベント
-		this.changeYmTb(this.ymTb);
 		
+		
+	}
+	
+	
+	/**
+	 * yyyy-mm--dd型に変換する
+	 * 
+	 * @note
+	 * 例) 2012-2-3 → 2012-02-03
+	 * 例) 2012-12-12 → 2012-12-12
+	 * 
+	 * @param string ym 年月文字列
+	 * @param string delim 区切り文字（デフォルトはハイフン）
+	 * @return yyyy-mm型の年月文字列
+	 */
+	_yyyymmdd(date_str, delim){
+		if(date_str == null) return date_str;
+		date_str = date_str.trim();
+		if(date_str == '' || date_str == 0) return date_str;
+		if(delim == null) delim = '-';
+		let ary = date_str.split(delim);
+		if (ary.length != 3) return date_str;
+		let month = ary[1];
+		month = ("0"+month).slice(-2); // 2桁にそろえる。 例 3→03
+		let day = ary[2];
+		day = ("0"+day).slice(-2); // 2桁にそろえる。 例 3→03
+		let date_str2 = ary[0] + delim + month + delim + day;
+		return date_str2;
 	}
 	
 	
@@ -252,9 +325,10 @@ class RangeYm{
 	 * @param string delim 区切り文字（デフォルトはハイフン）
 	 * @return yyyy-mm型の年月文字列
 	 */
-	_toYyyyMm(ym, delim){
+	_yyyymm(ym, delim){
+		if(ym == null) return ym;
 		ym = ym.trim();
-		if(ym == null || ym == '' || ym == 0) return ym;
+		if(ym == '' || ym == 0) return ym;
 		if(delim == null) delim = '-';
 		let ary = ym.split(delim);
 		if (ary.length != 2) return ym;
@@ -262,6 +336,21 @@ class RangeYm{
 		month = ("0"+month).slice(-2); // 2桁にそろえる。 例 3→03
 		let ym2 = ary[0] + delim + month;
 		return ym2;
+	}
+
+	
+	// Check empty.
+	_empty(v){
+		if(v == null || v == '' || v=='0'){
+			return true;
+		}else{
+			if(typeof v == 'object'){
+				if(Object.keys(v).length == 0){
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 
 	
