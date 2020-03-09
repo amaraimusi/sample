2cc1df49fe5fc3a57d6c7e3e7b9b0ad004c8dc26
user <amaraimusi@gmail.com>
2020-03-09 21:46:13 +0900

    PHPのサンプル：パスワード自動生成

---
+++ php/a060/password_auto_maker/password_auto_maker.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="google" content="notranslate" />
+   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+   	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>パスワード自動生成 | ワクガンス</title>
+	<link rel='shortcut icon' href='/home/images/favicon.ico' />
+	
+	<link href="/note_prg/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/note_prg/css/highlight/default.css" rel="stylesheet">
+	<link href="/note_prg/css/common2.css" rel="stylesheet">
+	<script src="/note_prg/js/jquery3.js"></script>	<!-- jquery-3.3.1.min.js -->
+	<script src="/note_prg/js/bootstrap.min.js"></script>
+	<script src="/note_prg/js/highlight.pack.js"></script>
+	<script src="/note_prg/js/livipage.js"></script>
+	<script src="/note_prg/js/ImgCompactK.js"></script>
+
+	<script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+<div id="header" ><h1>パスワード自動生成 | ワクガンス</h1></div>
+<div class="container">
+
+
+<h2>Demo</h2>
+<pre><code>
+// 8桁のパスワードをランダム作成（2種類以上の文字は選ばれない）
+$password = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 8);
+echo $password;
+</code></pre>
+<pre>
+<?php 
+// 8桁のパスワードをランダム作成（2種類以上の文字は選ばれない）
+$password = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 8);
+echo $password;
+?>
+</pre>
+<div class="yohaku"></div>
+<ol class="breadcrumb">
+	<li><a href="/">ホーム</a></li>
+	<li><a href="/sample">サンプルソースコード</a></li>
+	<li><a href="/sample/php">PHP ｜ サンプル</a></li>
+	<li>パスワード自動生成</li>
+</ol>
+</div><!-- content -->
+<div id="footer">(C) kenji uehara 2020-3-9</div>
+</body>
+</html>
