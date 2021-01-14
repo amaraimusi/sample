c3e1707212f06b2eeb1c1143e18e66e5ca7eae89
user <user@host.docker.internal>
2021-01-14 10:23:12 +0900

    TypeScriptのサンプル

--- type_script/hello_world/test.js
+++ type_script/hello_world/test.js
@@ -2,9 +2,10 @@ var HelloWorld = /** @class */ (function () {
     /** コンストラクタ
      */
     function HelloWorld(str1) {
-        this.BIG_PIG = '大きなブタ';
+        this.m_big_buta = '大きなブタ2'; // メンバ
         console.log(str1); //■■■□□□■■■□□□
     }
+    // publicメソッド
     HelloWorld.prototype.bark = function () {
         var value1 = 333; // 数値型の宣言
         var str1 = 'こんにちは世界！'; // 文字列型の宣言
@@ -12,12 +13,13 @@ var HelloWorld = /** @class */ (function () {
         console.log(str1);
         var obj1 = { neko: '猫', 'inu': '犬' }; // 普通の連想配列
         this._working(); // privateメソッドの呼び出し
-        console.log(this.BIG_PIG); // 定数のメンバを呼び出してみる
+        console.log(this.m_big_buta); // メンバを呼び出してみる
     };
+    // privateメソッド
     HelloWorld.prototype._working = function () {
         console.log('プライベートで猫が歩く'); //■■■□□□■■■□□□
     };
     return HelloWorld;
 }());
-var helloWorld = new HelloWorld("HelloWorld");
+var helloWorld = new HelloWorld("Hello World!");
 helloWorld.bark();
--- type_script/hello_world/test.ts
+++ type_script/hello_world/test.ts
@@ -1,32 +1,37 @@
+
+
+
 class HelloWorld
 {
-    m_big_buta: string = '大きなブタ'; // メンバ
+    m_big_buta: string = '大きなブタ2'; // メンバ
 
-	/** コンストラクタ
-	 */
-	constructor(str1: string){
-		console.log(str1);//■■■□□□■■■□□□
-	}
 
-	// publicメソッド
+    /** コンストラクタ
+     */
+    constructor(str1: string){
+        console.log(str1);//■■■□□□■■■□□□
+    }
+
+    // publicメソッド
     public bark()
     {
-		let value1: number = 333; // 数値型の宣言
-		let str1: string = 'こんにちは世界！'; // 文字列型の宣言
+        let value1: number = 333; // 数値型の宣言
+        let str1: string = 'こんにちは世界！'; // 文字列型の宣言
         console.log(value1);
         console.log(str1);
 
-		let obj1 = {neko:'猫','inu':'犬'}; // 普通の連想配列
-		
-		this._working(); // privateメソッドの呼び出し
+        let obj1 = {neko:'猫','inu':'犬'}; // 普通の連想配列
+        
+        this._working(); // privateメソッドの呼び出し
 
-		console.log(this.m_big_buta); // メンバを呼び出してみる
+        console.log(this.m_big_buta); // メンバを呼び出してみる
+        
     }
 
-	// privateメソッド
-	private _working(){
-		console.log('プライベートで猫が歩く');//■■■□□□■■■□□□
-	}
+    // privateメソッド
+    private _working(){
+        console.log('プライベートで猫が歩く');//■■■□□□■■■□□□
+    }
 }
 
 var helloWorld = new HelloWorld("Hello World!");
