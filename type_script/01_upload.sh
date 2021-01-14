7301dac0bf82a0e34c9b3f2cbd0660ed84643e46
user <user@host.docker.internal>
2021-01-14 11:08:35 +0900

    TypeScriptのサンプル：目次ページ

---
+++ type_script/01_upload.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+echo 'ソースコードを差分アップロードします。'
+
+rsync -auvz ../type_script amaraimusi@amaraimusi.sakura.ne.jp:www/sample/
+rsync -auvz ../index.html amaraimusi@amaraimusi.sakura.ne.jp:www/sample/
+
+echo "------------ 送信完了"
+#cmd /k
