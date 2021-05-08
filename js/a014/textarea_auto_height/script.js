e2e301e21983795ebb60c499268125ccc9d61153
user <user@host.docker.internal>
2021-05-08 11:49:29 +0900

    修正：テキストエリア(textarea)の高さ自動調整

--- js/a014/textarea_auto_height/script.js
+++ js/a014/textarea_auto_height/script.js
@@ -3,12 +3,21 @@
 jQuery(()=>{
 	
 	automateTextareaHeight('#textarea1');
+	automateTextareaHeight('#textarea2');
 });
 
 // テキストエリアの高さを自動調整する。
 function automateTextareaHeight(slt){
 
-	$(slt).attr("rows", 1).on("input", e => {
-		$(e.target).height(0).innerHeight(e.target.scrollHeight);
-	});
+		let taElm = $(slt);
+		
+		// 文字入力した時に高さ自動調整
+		taElm.attr("rows", 1).on("input", e => {
+			$(e.target).height(0).innerHeight(e.target.scrollHeight);
+		});
+		
+		// クリックしたときに自動調整
+		taElm.attr("rows", 1).click("input", e => {
+			$(e.target).height(0).innerHeight(e.target.scrollHeight);
+		});
 }
