710e2896c7f684c6b6ed436083a41bf6be18a55e
user <user@host.docker.internal>
2022-02-05 11:20:07 +0900

    JavaScriptのサンプル：URLからクエリを抜き出す | GETパラメータ | ワクガンス

---
+++ js/a015/get_query_from_url/script.js
@@ -0,0 +1,43 @@
+
+
+
+function test1(){
+	let url = $('#textbox1').val();
+	
+	let param =getQueryFromURL(url);
+	
+	// 検証結果に出力
+	console.log(param);
+	let json_str = JSON.stringify(param);
+	$('#res').html(json_str);
+	
+}
+
+
+/**
+ * URLからクエリパラメータを抜き出す | URLからGETパラメータの取得
+ * @param url
+ * @return object URLクエリデータ
+ */
+function getQueryFromURL(url){
+	
+	if (url==null || url==""){
+		return {};
+	}
+	
+	let a=url.indexOf('?');
+	let q_str =url.substring(a+1, url.length);
+		
+	if(q_str =='' || q_str==null) return {};
+	
+	q_str = q_str.substring(1, q_str.length);
+	let ary = q_str.split('&');
+	let data = {};
+	for(let i=0 ; i<ary.length ; i++){
+		let s = ary[i];
+		let prop = s.split('=');
+		data[prop[0]]=prop[1];
+	}	
+	
+	return data;
+}
